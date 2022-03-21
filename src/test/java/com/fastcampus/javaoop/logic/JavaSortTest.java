651a65541d119ec3478d14794a62ee5f353e52ff
slay001 <slay001>
2022-03-21 23:04:40 +0900

    #1 BubbleSort, JavaSort 구현

---
+++ src/test/java/com/fastcampus/javaoop/logic/JavaSortTest.java
@@ -0,0 +1,26 @@
+package com.fastcampus.javaoop.logic;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Slayra
+ * @date 2022-03-21
+ **/
+class JavaSortTest {
+
+    @Test
+    void 구현체를_사용한_정렬() {
+        // Given
+        JavaSort<Integer> javaSort = new JavaSort<>();
+
+        // When
+        List<Integer> actual = javaSort.sort(List.of(3,2,4,5,1));
+
+        // Then
+        assertEquals(List.of(1,2,3,4,5), actual);
+    }
+}
