b9848839960237fb85f50f1f80e8d529b5814a26
slay001 <slay001>
2022-03-24 18:29:09 +0900

    #3 서비스 분리 후 인터페이스 활용한 구현체 테스트 작성

---
+++ src/test/java/com/fastcampus/javaoop/service/SortServiceTest.java
@@ -0,0 +1,30 @@
+package com.fastcampus.javaoop.service;
+
+import com.fastcampus.javaoop.logic.BubbleSort;
+import com.fastcampus.javaoop.logic.JavaSort;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Slayra
+ * @date 2022-03-24
+ **/
+class SortServiceTest {
+
+    private SortService sut = new SortService(new JavaSort<>());
+
+    @Test
+    void test() {
+        // Given
+
+        // When
+        List<String> actual = sut.doSort(List.of("3", "2", "1"));
+
+        // Then
+        assertEquals(List.of("1", "2", "3"), actual);
+    }
+
+}
